537ac72f4bbd1d8f6d0d92cfd8ea8f18afe38bd5
Mxpea  ~a lazy one~ <a142311@outlook.com>
2025-11-14 16:56:38 +0800

    添加贴图，注释掉无用代码

--- src/main/java/io/github/mxpea/fadingshadow/item/ModItem.java
+++ src/main/java/io/github/mxpea/fadingshadow/item/ModItem.java
@@ -12,12 +12,15 @@ import net.neoforged.neoforge.registries.NeoForgeRegistries;
 public class ModItem {
     public static final DeferredRegister.Items ITEMS =
             DeferredRegister.createItems(FadingShadow.MODID);
-
+//在这里注册物品，复制就行
     public static final DeferredItem<Item> reality_fabric =
             ITEMS.register("reality_fabric", () -> new Item(new Item.Properties()));
+
     public static final DeferredItem<Item> apple_juice =
             ITEMS.register("apple_juice", () -> new Item(new Item.Properties()));
 
+
+    //下面是初始化的，不要动
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
