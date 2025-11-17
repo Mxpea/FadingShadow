5c212cabd258345c3fe81cb149d2d244c483db01
Mxpea  ~a lazy one~ <a142311@outlook.com>
2025-11-17 20:40:30 +0800

    添加geckolib前置,修改gradle配置

--- src/main/java/io/github/mxpea/fadingshadow/block/ModBlock.java
+++ src/main/java/io/github/mxpea/fadingshadow/block/ModBlock.java
@@ -12,22 +12,26 @@ import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.function.Supplier;
 
+/*
+author:Mxpea
+Edited:25.11.16
+ */
 public class ModBlock {
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(FadingShadow.MODID);
 
     public static final DeferredBlock<Block> scranton_reality_anchors =
-            registerBlocks("scranton_reality_anchors", () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)));
+            registerBlocks(() -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)));
 
 
     //注册方块物品
-    private static <T extends Block> void registerBlockItems(String name, DeferredBlock<T> block) {
-        ModItem.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    private static <T extends Block> void registerBlockItems(DeferredBlock<T> block) {
+        ModItem.ITEMS.register("scranton_reality_anchors", () -> new BlockItem(block.get(), new Item.Properties()));
     }
 
-    private static <T extends Block> DeferredBlock<T> registerBlocks(String name, Supplier<T> block) {
-        DeferredBlock<T> blocks = BLOCKS.register(name, block);
-        registerBlockItems(name, blocks);
+    private static <T extends Block> DeferredBlock<T> registerBlocks(Supplier<T> block) {
+        DeferredBlock<T> blocks = BLOCKS.register("scranton_reality_anchors", block);
+        registerBlockItems(blocks);
         return blocks;
     }
 
